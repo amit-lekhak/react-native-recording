476929aa78a3ab09f1d0d753c0ea6040cbce65f6
amit-lekhak <amitlekhak1@gmail.com>
2021-09-30 17:51:13 +0545

    use react native camera to record video & flip screen

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "tabWidth": 2,
+  "useTabs": false,
+  "printWidth": 120,
+  "endOfLine": "lf",
+  "singleQuote": true,
+  "arrowParens": "avoid",
+  "bracketSpacing": true,
+  "jsxSingleQuote": true,
+  "trailingComma": "all",
+  "bracketSameLine": false
+}
